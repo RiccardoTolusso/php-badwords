0350b4407a1c8004c8d88a4ab5cf85f511057ade
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-06-17 18:11:09 +0200

    [UPD] added some style to index.html

--- index.php
+++ index.php
@@ -5,23 +5,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>No Bad Words Allowed</title>
-    sty
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 
 <body>
-    <form action="./requests/get_badwords.php" method="get">
-        <div>
-            <label for="text">Inserisci qui il tuo testo</label>
-            <textarea name="text" id="text"></textarea>
+    <div class="vh-100 bg-secondary">
+        <div class="container text-center">
+
+            <div class="py-5">
+                <h1 class="display-1 fw-bold text-uppercase">Badwords Parser</h1>
+                <h5 class="display-6">Free Online Bad Words Parsers</h5>
+            </div>
+            <form action="./requests/get_badwords.php" method="get" class="py-4">
+                <div class="form-floating mb-4">
+                    <textarea name="text" id="text" class="form-control" placeholder="Inserisci il testo" required></textarea>
+                    <label for="text">Inserisci qui il tuo testo</label>
+                </div>
+
+                <div class="form-floating mb-4">
+                    <input type="text" name="badwords" id="badwords" class="form-control" placeholder="Inserisci le parole da filtrare" required>
+                    <label for="badwords">Inserisci qui le tue badwords separate da spazi es: "parola1 parola2"</label>
+                </div>
+
+                <button class="btn btn-success">Invia il testo da ripulire</button>
+
+            </form>
         </div>
-        <div>
-            <label for="badwords">Inserisci qui le tue badwords</label>
-            <input type="text" name="badwords" id="badwords">
-        </div>
-        <div>
-            <button>Invia il testo da ripulire</button>
-        </div>
-    </form>
+    </div>
+
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </body>
+
 
 </html>
